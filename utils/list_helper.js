b3d96cf77f8d4555eb867e56d1b473e1c4efefb4
Andreas Ranch <ranch@sowasound.com>
2022-12-17 12:10:43 +0100

    Added eslint files Eslint not working. Jeg vil prøve at nulstille vscode settings, og prøve igen

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,19 @@ const dummy = (blogs) => {
     const notUsed = blogs
     return 1
   }
+
+const listHelper = (blogs) () => {
+    if (emptyList.length === 0) {
+        return 0
+    } else if (blog.length === 1) {
+        return blog.likes
+    } else if (blog.length > 1 ) {
+        let likes = 0
+        blog.forEach(blog => {likes += blog.likes})
+        return likes
+}
+}
+
   
   module.exports = {
     dummy
