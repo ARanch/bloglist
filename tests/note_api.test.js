3976615d140ab8009acb8815a5831e94fc6f3392
Andreas Ranch <ranch@sowasound.com>
2022-12-20 20:44:02 +0100

    tests work – trying to refactor into test_helper function

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -2,32 +2,19 @@ const { createTestScheduler } = require('jest')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const notesRouter = require('../controllers/notes')
 const api = supertest(app)
 const Note = require('../models/note')
+const helper = require('./test_helper')
 
 // ==== 18/12/2022, 22.19  ==== 
 // make sure database is in same state when testing, always
 // note that this actually clears the MongoDB online atm.
-const initialNotes = [
-    {
-        content: 'HTML is easy',
-        date: new Date(),
-        important: false,
-    },
-    {
-        content: 'Browser can execute only Javascript',
-        date: new Date(),
-        important: true,
-    },
-]
-
 // use Jest beForeEarch() to clear and rewrite the database before test run
 beforeEach(async () => {
     await Note.deleteMany({}) // delete notes
-    let noteObject = new Note(initialNotes[0])
+    let noteObject = new Note(helper.initialNotes[0])
     await noteObject.save()
-    noteObject = new Note(initialNotes[1])
+    noteObject = new Note(helper.initialNotes[1])
     await noteObject.save()
 })
 // ========================================================
@@ -44,7 +31,7 @@ test('all notes are returned', async () => {
 
     // execution gets here only after the HTTP request is complete
     // the result of HTTP request is saved in variable response
-    expect(response.body).toHaveLength(initialNotes.length)
+    expect(response.body).toHaveLength(helper.initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
@@ -52,6 +39,44 @@ test('a specific note is within the returned notes', async () => {
 
     const contents = response.body.map(r => r.content)
     expect(contents).toContain('Browser can execute only Javascript')
+})
+
+// ==== 20/12/2022, 20.12  ==== 
+// test api for adding notes
+test('notes can be added', async () => {
+    const noteContent = 'test adding a new note'
+    const newNote = {
+        content: noteContent,
+        important: true
+    }
+    await api // posting new note 
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201) // "created"
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/notes')
+    const contents = response.body.map(r => r.content)
+
+    expect(response.body).toHaveLength(helper.initialNotes.length + 1)
+    expect(contents).toContain(noteContent)
+})
+
+test('empty note is not added', async () => {
+    const emptyNote = {
+        important: false
+    }
+
+    await api
+        .post('/api/notes')
+        .send(emptyNote)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/notes')
+    // const allNotes = helper.notesInDb()
+    // expect(response.body).toHaveLength(helper.initialNotes.length)
+    // expect(response.body).toHaveLength(helper.initialNotes.length)
 })
 
 afterAll(() => {
