1c4ad76037565171b354ce67b32fccd16fb0bc94
Andreas Ranch <ranch@sowasound.com>
2022-12-22 23:52:27 +0100

    starting on adding populate method

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,5 +1,6 @@
 const Note = require('../models/note')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialNotes = [
     {
@@ -47,10 +48,16 @@ const blogsInDb = async () => {
     return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
     initialNotes,
     initialBlogs,
     nonExistingId,
     notesInDb,
-    blogsInDb
+    blogsInDb,
+    usersInDb
 }
