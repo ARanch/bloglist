71ffada9dffcd5ecde2c776cdeedfd3aeea4fa78
Andreas Ranch <ranch@sowasound.com>
2022-12-24 00:37:31 +0100

    test 400 when name is short

---
+++ controllers/users.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcrypt')
+const { json } = require('express')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const passwordRequirement = 3
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({})
+    .populate('notes', { content: 1, date: 1 }) // object parameter chooses fields in note
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { userName, name, password } = request.body
+
+  if (!userName) {
+    return response.status(400).json({ error: 'must provide a username' })
+  } else if (userName.length < 3) {
+    return response.status(400).json({ error: 'username must be more than 3 characters long' })
+  } else if (await User.findOne({ userName: userName })) {
+    return response.status(409).json({ error: 'username already exists' })
+  }
+
+  if (!password) {
+    return response.status(400).json({ error: 'no password provided' })
+  } else if (password.length < passwordRequirement) {
+    return response.status(400).json({
+      error:
+        `password must be at least ${passwordRequirement} character long`
+    })
+  }
+
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    userName,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
---
+++ tests/user.test.js
@@ -0,0 +1,79 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", false);
+const helper = require('./test_helper')
+const app = require('../app')
+
+const api = supertest(app)
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('username must be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+})
+
+describe('when users are added', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+  })
+  test.only('returns 400 when name is less than 3 chars long', () => {
+    const newUser = {
+      userName: 'ro',
+      name: 'Superuser',
+      password: 'ohyeah'
+    }
+    api
+      .post('api/users')
+      .send(newUser)
+      .expect(400)
+  })
+}) 
